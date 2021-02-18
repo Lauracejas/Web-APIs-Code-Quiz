bb315a212b9692dc26bc15d1e5f9faff35fa060c
lauracejas <lauracejasmartin94@gmail.com>
2021-02-18 16:34:56 -0500

    Updated questions Array and updated the question click

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,31 +1,31 @@
 var questions = [
     {
         title: "The # symbol specifies that the selector is?",
-        choices: { a: "class", b: "tags", c: "id", d: "css" },
-        answer: "c"
+        choices: [ "class", "tags", "id", "css" ],
+        answer: "id"
     },
     {
         title: "Which HTML tag is used to define an internal style sheet?",
-        choices: { a: "<css>", b: "<body>", c: "<script>", d: "<style>" },
-        answer: "d"
+        choices: [  "<css>", "<body>", "<script>", "<style>" ],
+        answer: "<style>"
     },
     {
         title: "The coding languages Java and JavaScript are the same",
-        choices: { a: "true", b: "false" },
-        answer: "b"
+        choices: [  "true", "false" ],
+        answer: "false"
     },
     {
         title: " How do you write 'Hello World' in an alert box?",
-        choices: {
-            a: "msgBox('Hello World')",
-            b: "alertBox('Hello World')",
-            c: "msg('Hello World')",
-            d: "alert('Hello World')"
-        },
-        answer: "d"
+        choices: [ 
+             "msgBox('Hello World')",
+            "alertBox('Hello World')",
+             "msg('Hello World')",
+             "alert('Hello World')"
+],
+        answer: "alert('Hello World')"
     },
     {
         title: "What does the following expression return? '!false'",
-        choices: { a: "null", b: "true", c: "undefined", d: "false" },
-        answer: "c"
+        choices:["null", "true", "undefined", "false" ],
+        answer: "undefined"
     }]
--- Assets/script.js
+++ Assets/script.js
@@ -7,24 +7,69 @@ var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
 var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
+var questionIndex = 0
 
 //Start the Quiz
 startBtn.addEventListener("click", function () {
     console.log("start the game");
+    // hide start screen
+    var startScreenEl = document.getElementById("start-quiz");
+    startScreenEl.setAttribute("class", "hide");
+
+    // Un-hide questions
+    questionsEl.removeAttribute("class");
+
     //start the timer
-    function setTime() {       
+    function setTime() {
         var timerInterval = setInterval(function () {
-           
+
             secondsLeft--;
             timeleft.textContent = secondsLeft;
-            console.log(secondsLeft);
+            //console.log(secondsLeft);
             if (secondsLeft === 0) {
                 clearInterval(timerInterval);
             }
         }, 1000);
-      //  firstQuestion();
+
+        firstQuestion();
     }
     setTime();
 });
+
+
+function firstQuestion() {
+
+    var questionNext = questions[questionIndex];
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = questionNext.title;
+    choicesEl.innerHTML = "";
+
+    //loop over choices
+    questionNext.choices.forEach(function (choice, i) {
+        var choiceButton = document.createElement("button");
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+        choiceButton.textContent = i + 1 + "." + choice;
+        choiceButton.addEventListener("click", questionClick());
+        choicesEl.appendChild(choiceButton);
+    });
+}
+
+function questionClick() {
+    if (this.value !== questions[questionIndex].answer) {
+        secondsLeft -= 10;
+        if (secondsLeft < 0) {
+            secondsLeft = 0;
+        }
+
+    }
+    questionIndex++;
+    if (questionIndex === questions.length) {
+        quizEnd();
+    } else {
+        firstQuestion();
+    }
+}
+
 
 
